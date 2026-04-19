20db1ba5333572df7a2f010c665f71e0b32449e8
maximo12345678901 <maximozweers@gmail.com>
2026-04-20 01:18:09 +0200

    window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include <cmath>
+#include "../vec.h"
+#include "../ui.h"
 
 int main() {
+  uint windowWidth = 1600;
+  uint windowHeight = 900;
+  
+  sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "blac hol");
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
   return 0;
 }
